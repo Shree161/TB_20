31bb0165e57a9477ffc45d01ea6cf8fad74c14dd
swati-patill <patil.swati0821@gmail.com>
2023-05-16 12:04:33 +0530

    by pqr

--- GitProjectDemo1/src/com/Demo1.java
+++ GitProjectDemo1/src/com/Demo1.java
@@ -4,7 +4,11 @@ public class Demo1
 {
 	public void m1()
 	{
-		System.out.println("hello m1"):
+		System.out.println("hello m1");
+	}
+	public void m2()
+	{
+		System.out.println("hello m2");
 	}
 	public static void main(String[] args) 
 	{
